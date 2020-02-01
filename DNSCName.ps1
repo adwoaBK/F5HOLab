48abbdc40b55e62e7db63e4f7c8e02cc22f1c41c
Jeevan Bisht <jeevanb@hotmail.com>
2020-01-31 16:37:09 -0800

    Master
    Master

---
+++ DNSCName.ps1
@@ -0,0 +1,23 @@
+﻿$CNAME= "1aabc-wingtiptoyssite.msappproxy.net"
+$name="1abc"                                  
+
+#=============================================================================================
+# Donot Modify anything below this
+#=============================================================================================
+
+$domain="wingtiptoys.site"                           # your domain
+$type="CNAME"                                    # Record type A, CNAME, MX, etc.
+$ttl=600                                   # Time to Live min value 600
+$port=1                                    # Required port,` Min value 1
+$weight=1                                  # Required weight, Min value 1
+$key = 'e4hZ7JiM2EYH_VcsT54yNqgatTvtPZc15n3' #key for godaddy developer API
+$secret = 'JWSjjivUHk4x6iNPTMyvzw' #Secret for godday developer API
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 
+
+$headers = @{}
+
+$headers["Content-Type"] = 'application/json'
+$headers["Authorization"] = 'sso-key ' + $key + ':' + $secret
+$Request = @(@{data=$CNAME;port=$port;priority=0;protocol="string";service="string";ttl=$ttl;weight=$weight })
+$JSON = Convertto-Json $request
+$result = Invoke-WebRequest https://api.godaddy.com/v1/domains/$domain/records/$type/$name -method put -headers $headers -Body $json -ContentType "application/json" -UseBasicParsing
